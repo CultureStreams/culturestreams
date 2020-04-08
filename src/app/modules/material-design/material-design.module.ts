45b057cff50be50ae311c96d16e917dc955127d7
BlitzundBogen <19719979+BlitzundBogen@users.noreply.github.com>
2020-04-08 19:26:58 +0200

    updated material design module

--- src/app/modules/material-design/material-design.module.ts
+++ src/app/modules/material-design/material-design.module.ts
@@ -2,36 +2,37 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatCheckboxModule} from '@angular/material/checkbox';
+// import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatRadioModule} from '@angular/material/radio';
+// import {MatRadioModule} from '@angular/material/radio';
 import {MatSelectModule} from '@angular/material/select';
 import {MatSliderModule} from '@angular/material/slider';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+// import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatListModule} from '@angular/material/list';
-import {MatGridListModule} from '@angular/material/grid-list';
+// import {MatGridListModule} from '@angular/material/grid-list';
 import {MatCardModule} from '@angular/material/card';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatChipsModule} from '@angular/material/chips';
+// import {MatStepperModule} from '@angular/material/stepper';
+// import {MatTabsModule} from '@angular/material/tabs';
+// import {MatExpansionModule} from '@angular/material/expansion';
+// import {MatButtonToggleModule} from '@angular/material/button-toggle';
+// import {MatChipsModule} from '@angular/material/chips';
 import {MatIconModule} from '@angular/material/icon';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatTooltipModule} from '@angular/material/tooltip';
+// import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+// import {MatProgressBarModule} from '@angular/material/progress-bar';
+// import {MatDialogModule} from '@angular/material/dialog';
+// import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatTableModule} from '@angular/material/table';
-import {MatSortModule} from '@angular/material/sort';
-import {MatPaginatorModule} from '@angular/material/paginator';
+// import {MatTableModule} from '@angular/material/table';
+// import {MatSortModule} from '@angular/material/sort';
+// import {MatPaginatorModule} from '@angular/material/paginator';
 // TODO comment unnecessary ones out
 
 @NgModule({
@@ -39,71 +40,71 @@ import {MatPaginatorModule} from '@angular/material/paginator';
   imports: [
     CommonModule,
     BrowserAnimationsModule,
-    MatCheckboxModule,
-    MatCheckboxModule,
+    // MatCheckboxModule,
     MatButtonModule,
     MatInputModule,
     MatAutocompleteModule,
     MatDatepickerModule,
+    MatNativeDateModule,
     MatFormFieldModule,
-    MatRadioModule,
+    // MatRadioModule,
     MatSelectModule,
     MatSliderModule,
-    MatSlideToggleModule,
+    // MatSlideToggleModule,
     MatMenuModule,
     MatSidenavModule,
     MatToolbarModule,
     MatListModule,
-    MatGridListModule,
+    // MatGridListModule,
     MatCardModule,
-    MatStepperModule,
-    MatTabsModule,
-    MatExpansionModule,
-    MatButtonToggleModule,
-    MatChipsModule,
+    // MatStepperModule,
+    // MatTabsModule,
+    // MatExpansionModule,
+    // MatButtonToggleModule,
+    // MatChipsModule,
     MatIconModule,
-    MatProgressSpinnerModule,
-    MatProgressBarModule,
-    MatDialogModule,
-    MatTooltipModule,
+    // MatProgressSpinnerModule,
+    // MatProgressBarModule,
+    // MatDialogModule,
+    // MatTooltipModule,
     MatSnackBarModule,
-    MatTableModule,
-    MatSortModule,
-    MatPaginatorModule
+    // MatTableModule,
+    // MatSortModule,
+    // MatPaginatorModule
   ],
   exports: [
     BrowserAnimationsModule,
-    MatCheckboxModule,
-    MatCheckboxModule,
+    // MatCheckboxModule,
     MatButtonModule,
     MatInputModule,
     MatAutocompleteModule,
     MatDatepickerModule,
+    MatNativeDateModule,
     MatFormFieldModule,
-    MatRadioModule,
+    // MatRadioModule,
     MatSelectModule,
     MatSliderModule,
-    MatSlideToggleModule,
+    // MatSlideToggleModule,
     MatMenuModule,
     MatSidenavModule,
     MatToolbarModule,
     MatListModule,
-    MatGridListModule,
+    // MatGridListModule,
     MatCardModule,
-    MatStepperModule,
-    MatTabsModule,
-    MatExpansionModule,
-    MatButtonToggleModule,
-    MatChipsModule,
+    // MatStepperModule,
+    // MatTabsModule,
+    // MatExpansionModule,
+    // MatButtonToggleModule,
+    // MatChipsModule,
     MatIconModule,
-    MatProgressSpinnerModule,
-    MatProgressBarModule,
-    MatDialogModule,
-    MatTooltipModule,
+    // MatProgressSpinnerModule,
+    // MatProgressBarModule,
+    // MatDialogModule,
+    // MatTooltipModule,
     MatSnackBarModule,
-    MatTableModule,
-    MatSortModule,
-    MatPaginatorModule
+    // MatTableModule,
+    // MatSortModule,
+    // MatPaginatorModule
   ]
 })
 export class MaterialDesignModule { }
