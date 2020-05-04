d459f8973393cd28056f1fbb8fe79f513ae559ad
BlitzundBogen <19719979+BlitzundBogen@users.noreply.github.com>
2020-05-04 06:40:46 +0200

    solving problem with dates in week, improving layout

--- src/app/features/home/pages/calendar/event-item/event-item.component.ts
+++ src/app/features/home/pages/calendar/event-item/event-item.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { DataStore } from 'src/services/data.service';
 import { Organizer } from 'src/models/organizer';
 
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'cs-event-item',
   templateUrl: './event-item.component.html',
@@ -14,11 +16,17 @@ export class EventItemComponent implements OnInit {
   protected organizers: Organizer[] = [];
   protected date: Date;
 
-  constructor( protected dataStore: DataStore) {
+  constructor(
+    protected router: Router,
+    protected dataStore: DataStore) {
     this.date = new Date();
    }
 
   ngOnInit() {
+  }
+
+  protected navigateToEvent(event) {
+    this.router.navigate(['/event', event.id]);
   }
 
 }
