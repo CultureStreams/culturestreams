8da19f7a4f111eafb4d8191912484cad0304c6d3
BlitzundBogen <19719979+BlitzundBogen@users.noreply.github.com>
2020-05-22 22:33:26 +0200

    bugfix homerouting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+  // { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '',
     loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule)
   },
--- src/app/features/home/home.routing.ts
+++ src/app/features/home/home.routing.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from "./home.component";
+
 import { StartComponent } from './pages/start/start.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 
 const homeRoutes: Routes = [
-  { path: '', component:  StartComponent,
+  { path: '', component:  HomeComponent,
     children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: StartComponent },
       { path: 'about', component: AboutComponent },
       { path: 'contact', component: ContactComponent }
     ]
@@ -21,6 +25,7 @@ const homeRoutes: Routes = [
 })
 export class HomeRoutingModule { }
 export const homeRoutingComponents = [
+  HomeComponent,
   StartComponent,
   AboutComponent,
   ContactComponent
