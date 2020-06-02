1fb2e943855d1ab03b4cce5f84bda2a942037c98
Carina <carinalanderer@web.de>
2020-06-02 11:43:19 +0200

    deployment stuff

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule, homeRoutingComponents } from './home.routing';
 
 import { SharedModule } from "@shared/shared.module";
+import { ImprintComponent } from './pages/imprint/imprint.component';
 
 @NgModule({
   declarations: [
-    homeRoutingComponents
+    homeRoutingComponents,
+    ImprintComponent
   ],
   imports: [
     CommonModule,
